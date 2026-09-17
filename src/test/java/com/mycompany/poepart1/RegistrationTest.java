2ad438cef22009b323fd3247c7832bd9d6cb2b72
Student <Student@LabVM2049939.zl1yfvyc1ksudlbasvt320s22a.jnbx.internal.cloudapp.net>
2026-09-17 09:08:36 +0200

    password complexity test

--- src/test/java/com/mycompany/poepart1/RegistrationTest.java
+++ src/test/java/com/mycompany/poepart1/RegistrationTest.java
@@ -36,6 +36,41 @@ public class RegistrationTest {
         Registration reg = new Registration("John", "Doe", "kyle_123", "Password123!", "+27821234567");
         assertFalse(reg.checkUserName());
     }
+  //PASSWORD COMPLEXITY TESTS
+   
+    @Test
+    public void testCheckPasswordComplexity_Valid() {
+        // >= 8 chars, includes uppercase, digit, and special char
+        Registration reg = new Registration("John", "Doe", "kyl_1", "Ch3&sec@ke", "+27821234567");
+        assertTrue(reg.checkPasswordComplexibility());
+    }
 
+    @Test
+    public void testCheckPasswordComplexity_Invalid_TooShort() {
+        // Less than 8 characters
+        Registration reg = new Registration("John", "Doe", "kyl_1", "Ch3&s", "+27821234567");
+        assertFalse(reg.checkPasswordComplexibility());
+    }
+
+    @Test
+    public void testCheckPasswordComplexity_Invalid_NoCapital() {
+        // Missing capital letter
+        Registration reg = new Registration("John", "Doe", "kyl_1", "ch3&sec@ke", "+27821234567");
+        assertFalse(reg.checkPasswordComplexibility());
+    }
+
+    @Test
+    public void testCheckPasswordComplexity_Invalid_NoNumber() {
+        // Missing digit
+        Registration reg = new Registration("John", "Doe", "kyl_1", "Chee&sec@ke", "+27821234567");
+        assertFalse(reg.checkPasswordComplexibility());
+    }
+
+    @Test
+    public void testCheckPasswordComplexity_Invalid_NoSpecialChar() {
+        // Missing special character
+        Registration reg = new Registration("John", "Doe", "kyl_1", "Ch33secake", "+27821234567");
+        assertFalse(reg.checkPasswordComplexibility());
+    }
    
     
